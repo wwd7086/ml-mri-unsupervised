07c7233ad0c2894e3a32ff2fd7d672b3649f6d6f
Wenda Wang <wendaw@unix4.andrew.cmu.edu>
2015-12-13 23:21:34 -0500

    run kmean

--- kmeanRecon.m
+++ kmeanRecon.m
@@ -1,20 +1,24 @@
 function missVoxel = kmeanRecon(numPC, train,testProv,missIdx,provideIdx)
 
 % learn the dictionary
-nReplicates     = 10;
-[~,centers,~,~] = kmeans_sparsified( train', numPC,'ColumnSamples',true,...
-    'Display','off','Replicates',nReplicates,'Sparsify',false, 'start','++');
+addpath('KMeans++');
+setup_kmeans;
+nReplicates     = 5;
+%[~,centers,~,~] = kmeans_sparsified( train', numPC,'ColumnSamples',true,...
+%    'Display','off','Replicates',nReplicates,'Sparsify',false, 'start','++');
+%save('kmean.mat','centers');
+load('kmean.mat');
 
 %centers k*p
 numTest = size(testProv,1);
 missVoxel = zeros(numTest,size(missIdx,2));
 for i=1:numTest
     % learn weight
-    cprov = centers(:,provideIdx)';
+    cprov = centers(provideIdx,:);
     w = cprov\(testProv(i,:))';
     
     % predict missing value
-    cmiss = centers(:,missIdx)';
+    cmiss = centers(missIdx,:);
     missVoxel(i,:) = (cmiss*w)';
 end
 
