d0a1a99a5a5d950f96f5da218c63ee65086ca51f
Wang Wenda <wwd7086@gmail.com>
2015-12-14 00:23:33 -0500

    sparse

--- baseline.m
+++ baseline.m
@@ -1,25 +1,36 @@
 clear;
 close all;
 
+% load datas
 load('full.mat');
 load('provideIdx.mat');
 load('missIdx.mat');
 load('provideData_1000.mat');
 
+% load libs
+addpath spams-matlab;
+start_spams;
+addpath KMeans++;
+
 %% inputs
-doTest = true;
-doGen = false;
-
-train = X2train;
-testProv = X2test(:,provideIdx);
-testMiss = X2test(:,missIdx);
-
-%train = [X2train;X2test];
-%testProv = provideData;
+isTest = true;
+if isTest
+    doTest = true;
+    doGen = false;
+    train = X2train;
+    testProv = X2test(:,provideIdx);
+    testMiss = X2test(:,missIdx);
+else
+    doTest = false;
+    doGen = true;
+    train = [X2train;X2test];
+    testProv = provideData;
+end
 
 %% learn PCA from full voxel images
 %missVoxel = simplePCA(80,train,testProv,missIdx,provideIdx);
-missVoxel = kmeanRecon(200,train,testProv,missIdx,provideIdx);
+%missVoxel = kmeanRecon(200,train,testProv,missIdx,provideIdx);
+missVoxel = superSparse(200,train,testProv,missIdx,provideIdx);
 
 %% calculate erro
 if doTest
---
+++ superSparse.m
@@ -0,0 +1,38 @@
+function missVoxel = superSparse(numPC, train,testProv,missIdx,provideIdx)
+    %% learn a dictionary
+    param.K=256;  % learns a dictionary with 100 elements
+    param.lambda=0.15;
+    param.numThreads=-1; % number of threads
+    param.batchsize=400;
+    param.verbose=false;
+    param.iter=1000;  % let us see what happens after 1000 iterations.
+    
+    % D m*p  m->num of features
+    % train' m*n
+    tic
+    D = mexTrainDL_Memory(train',param); %m*p
+    t=toc;
+    fprintf('time of computation for Dictionary Learning: %f\n',t);
+    save('sparse.mat','D');
+
+    
+    %% find sparse coding using dictionary
+    % parameter of the optimization procedure are chosen
+    param.L=20; % not more than 20 non-zeros coefficients (default: min(size(D,1),size(D,2)))
+    param.eps=0.01; %
+    param.numThreads=-1; % number of processors/cores to use; the default choice is -1
+                     % and uses all the cores of the machine
+    
+    mask = zeros(size(testProv'));
+    mask(provideIdx,:) = 1;
+    tic
+    % alpha p*n
+    alpha=mexOMPMask(testProv',D,mask,param);
+    t = toc;
+    fprintf('%f signals processed per second\n',size(testProv,1)/t);
+    
+    % reconstruct
+    allVoxel = D*alpha %m*p*p*n = m*n
+    missVoxel = allVoxel(missIdx,:)';
+
+end
