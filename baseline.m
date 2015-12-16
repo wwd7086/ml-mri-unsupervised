d4c203b2fe684af1cd6fd9f9237957ba03ada31b
johnny <johnnywang@gmail.com>
2015-12-15 22:49:51 -0500

    Changes needed.

--- baseline.m
+++ baseline.m
@@ -8,32 +8,37 @@ load('missIdx.mat');
 load('provideData_1000.mat');
 
 % load libs
+addpath spams-matlab/build;
 addpath spams-matlab;
 start_spams;
 addpath KMeans++;
+addpath libsvm-3.21/matlab/;
 
 %% inputs
 isTest = false;
 if isTest
     doTest = true;
     doGen = false;
-    train = X2train;
+    vtrain = X2train;
     testProv = X2test(:,provideIdx);
     testMiss = X2test(:,missIdx);
 else
     doTest = false;
     doGen = true;
-    train = [X2train;X2test];
+    vtrain = [X2train;X2test];
     testProv = provideData;
 end
 
 %% learn PCA from full voxel images
-%missVoxel = simplePCA(80,train,testProv,missIdx,provideIdx);
-%missVoxel = eventPCA(80,train,testProv,missIdx,provideIdx);
-%missVoxel = kmeanRecon(200,train,testProv,missIdx,provideIdx);
-%missVoxel = superSparse(200,train,testProv,missIdx,provideIdx);
-%missVoxel = nnSearch(200,train,testProv,missIdx,provideIdx);
-%missVoxel = simpleRegression(80,train,testProv,missIdx,provideIdx);
+%missVoxel = simplePCA(80,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = eventPCA(80,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = kmeanRecon(200,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = superSparse(200,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = nnSearch(200,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = simpleRegression(80,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = svRegression(80,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = svliRegression(80,vtrain,testProv,missIdx,provideIdx);
+%missVoxel = superNeural(80,vtrain,testProv,missIdx,provideIdx);
 %missVoxel = simpleLaplacian(80,train,testProv,missIdx,provideIdx);
 %missVoxel = simplePCA_SVR(65,train,provideData,missIdx,provideIdx);
 missVoxel = simpleSVR(65,train,provideData,missIdx,provideIdx);
--- simpleRegression.m
+++ simpleRegression.m
@@ -3,7 +3,5 @@ function missVoxel = simpleRegression(numPC, train,testProv,missIdx,provideIdx)
 trainProv = train(:,provideIdx);
 trainMiss = train(:,missIdx);
 w = trainProv\trainMiss;
-%w = mvregress(trainProv,trainMiss, 'algorithm', 'cwls');
-missVoxel = testProv * w;
 
 end
