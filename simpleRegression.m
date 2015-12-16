35c0d0c94d8461cab6f8d0cc88f440d552198298
johnny <johnnywang@gmail.com>
2015-12-15 22:52:08 -0500

    New changes.

--- simpleRegression.m
+++ simpleRegression.m
@@ -1,7 +1,10 @@
 function missVoxel = simpleRegression(numPC, train,testProv,missIdx,provideIdx)
 
 trainProv = train(:,provideIdx);
+trainProv = [ones(size(trainProv,1),1),trainProv];
 trainMiss = train(:,missIdx);
 w = trainProv\trainMiss;
+%w = mvregress(trainProv,trainMiss,'algorithm','mvn');
+missVoxel = [ones(size(testProv,1),1),testProv] * w;
 
 end
